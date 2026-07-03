5e72eb9a01b4620ac994442fc07177de3bc0bab8
aldia_mechdream <futami16237@gmail.com>
2026-07-03 09:12:37 +0800

    chore：转换为tsdown； 取消很多不需要的打包依赖

---
+++ tsdown.config.ts
@@ -0,0 +1,79 @@
+import { defineConfig } from 'tsdown';
+import replace from '@rollup/plugin-replace';
+import funcMacro from 'rollup-plugin-func-macro';
+import pkg from './package.json';
+
+const isDev = process.env.NODE_ENV === 'development';
+const replacePlugin = () =>
+  replace({
+    preventAssignment: true,
+    delimiters: ['', ''],
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+
+    // global $throw
+    "$throw('": `throw new Error('[fastify-injecorator error] `,
+    '$throw(`': 'throw new Error(`[fastify-injecorator error] ',
+    '$throw("': `throw new Error("[fastify-injecorator error] `,
+    'declare const __IS_PROD__: boolean;\n': '',
+    'const __IS_PROD__: boolean;\n': '',
+    'logger.info(': "console.log(`%cinfo - __func__:`, 'color:#007ACC',",
+    'logger.warn(': "console.log(`%cwarn - __func__:`, 'color:#ff9900',",
+    'logger.error(': "console.log(`%cerror - __func__:`, 'color:#fb2c36',",
+    'logger.debug(': "console.log(`%cdebug - __func__:`, 'color:#8617a5',",
+    'logger.succ(': "console.log(`%cdebug - __func__:`, 'color:#00a00b',",
+    'logger.verbose(': "console.log(`%cdebug - __func__:`, 'color:#10aaaf',",
+    'logger.WorkspaceNotFound(': "console.log(`%cerror - __func__:`, 'color:#fb2c36','Workspace not found, id:',",
+    'logger.TabNotFoundInWorkspace(':
+      "console.log(`%cerror - __func__:`, 'color:#fb2c36','Tab not found in workspace. tabid,workspaceid:',",
+  });
+
+// Format date for package info
+function formatDateFull(dt = new Date()) {
+  const y = dt.getFullYear();
+  const m = String(dt.getMonth() + 1).padStart(2, '0');
+  const d = String(dt.getDate()).padStart(2, '0');
+  const hh = String(dt.getHours()).padStart(2, '0');
+  const mm = String(dt.getMinutes()).padStart(2, '0');
+  const ss = String(dt.getSeconds()).padStart(2, '0');
+  const ms = String(dt.getMilliseconds()).padStart(3, '0');
+  return `${y}.${m}.${d} ${hh}:${mm}:${ss}.${ms}`;
+}
+
+const __KEBAB_NAME__ = (process.env.KSKB_TSUMUGI_REAL_NAME ?? '').replace('rollup-plugin-', '');
+const __NAME__ = __KEBAB_NAME__.replace(/(^|-)(\w)/g, (_, __, c) => c.toUpperCase());
+
+const __PKG_INFO__ = `## About
+ * @package ${__NAME__}
+ * @author ${pkg.author.name} <${pkg.author.email}>
+ * @version ${pkg.version} (Last Update: ${formatDateFull()})
+ * @license ${pkg.license}
+ * @link ${pkg.repository.url}
+ * @link https://baendlorel.github.io/ Welcome to my site!
+ * @description ${pkg.description.replace(/\n/g, '\n * \n * ')}
+ * @copyright Copyright (c) ${new Date().getFullYear()} ${pkg.author.name}. All rights reserved.`;
+
+// External dependencies
+const external = [
+  'cron-parser',
+  // ...Object.keys(pkg.dependencies || {}),
+  // ...Object.keys(pkg.peerDependencies || {}),
+];
+
+export default defineConfig({
+  entry: [{ index: 'src/index.ts' }], // { cli: 'src/cli/index.ts' }
+  format: ['esm', 'cjs'],
+  external,
+  dts: true,
+  sourcemap: false,
+  alias: {
+    '@/': './src/',
+  },
+  define: {
+    __IS_DEV__: 'false',
+    __NAME__,
+    __KEBAB_NAME__,
+    __PKG_INFO__,
+  },
+  minify: true,
+  plugins: [replacePlugin(), funcMacro()],
+});
